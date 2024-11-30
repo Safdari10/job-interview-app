db466370c9cc30e5b12c6bd6927dc04ea5134713
Daniel <danielw@missionreadyhq.com>
2024-11-30 21:01:06 +1300

    created stae variables, import JobTitleInput component and created title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import JobTitleInput from "@/components/JobTitleInput";
+import { useState } from "react";
+
 export default function HomePage() {
+  const [jobTitle, setJobTitle] = useState("");
+  const [conversation, setConversation] = useState([]);
+
+  console.log(jobTitle);
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Welcome to My Next.js App</h1>
+    <main className="flex flex-col gap-2 items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Ai Mock Interviwer</h1>
+      <JobTitleInput jobTitle={jobTitle} setJobTitle={setJobTitle} />
     </main>
   );
 }
