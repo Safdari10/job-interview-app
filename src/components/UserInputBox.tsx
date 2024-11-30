5186ff92842f8e58527f8d391ce99c6550773270
Daniel <danielw@missionreadyhq.com>
2024-11-30 21:13:48 +1300

    created userInputBox component, eventually pass input to backend

--- src/components/UserInputBox.tsx
+++ src/components/UserInputBox.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+interface UserInputBoxProps {
+  userInput: string;
+  setUserInput: (userInput: string) => void;
+}
 
-export default function UserInputBox() {
-  return <div></div>;
+export default function UserInputBox({
+  userInput,
+  setUserInput,
+}: UserInputBoxProps) {
+  const handleSubmit = () => {
+    setUserInput("");
+  };
+
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
