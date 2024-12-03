f7e6401687ed8cd01be28c76745bd22957af3323
Zaker Safdari <zakersafdari@gmail.com>
2024-12-03 22:18:00 +1300

    added more styles

--- src/components/JobTitleInput.tsx
+++ src/components/JobTitleInput.tsx
@@ -9,7 +9,7 @@ export default function JobTitleInput({
 }: JobTitleInputProps) {
   return (
     <div className="mb-4">
-      <label htmlFor="job-title" className="text-xl text-[#0f9ed5]  font-bold mr-2">
+      <label htmlFor="job-title" className="text-xl text-[#0f9ed5] font-bold mr-2">
         Job Title:
       </label>
       <input
@@ -18,7 +18,7 @@ export default function JobTitleInput({
         onChange={(e) => setJobTitle(e.target.value)}
         id="job-title"
         name="job-title"
-        className="border-[#0f9ed5] border-solid border-2 p-1 w-80 rounded"
+        className="border-[#0f9ed5] border-solid border-2 p-2 w-80 rounded-md focus:outline-none focus:ring-2 focus:ring-[#0f9ed5] focus:border-transparent transition duration-300"
       />
     </div>
   );
