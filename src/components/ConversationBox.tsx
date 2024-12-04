66620d8912ae260c55f4998abcd9da8078af052b
Daniel <danielw@missionreadyhq.com>
2024-12-04 15:08:05 +1300

    imported the name and used it as required.

--- src/components/ConversationBox.tsx
+++ src/components/ConversationBox.tsx
@@ -1,5 +1,10 @@
+interface UserName {
+  name: string;
+  setName: (name: string) => void;
+}
+
 interface Conversation {
-  sender: "Interviewer" | "Me";
+  sender: "Interviewer" | UserName;
   text: string;
 }
 
@@ -9,16 +14,22 @@ interface ConversationProps {
 
 export default function ConversationBox({ conversation }: ConversationProps) {
   return (
-    <div className="border-2 border-solid border-gray-300 h-[600px] w-[900px] rounded-lg overflow-hidden hover:overflow-y-scroll bg-white shadow-md shadow-gray-400 p-4 custom-scrollbar">
+    <div className="border-2 border-solid border-gray-300 h-[600px] w-[1000px] rounded-lg overflow-hidden overflow-y-scroll bg-white shadow-md shadow-gray-400 p-4 custom-scrollbar">
       {conversation.map((convo, index) => (
         <div
           key={index}
-          className={`flex gap-2 p-3 text-lg mb-3 w-[850px] rounded-lg shadow ${
+          className={`flex flex-wrap gap-2 p-3 text-lg mb-6 mx-6 rounded-lg shadow max-w-fit ${
             convo.sender === "Interviewer"
               ? " text-[#0f9ed5] bg-gray-100"
               : " bg-blue-50"
-          }`}>
-          <p className="font-semibold">{convo.sender}:</p>
+          }`}
+        >
+          <p className="font-semibold">
+            {typeof convo.sender === "string"
+              ? convo.sender
+              : convo.sender.name}
+            :
+          </p>
           <p>{convo.text}</p>
         </div>
       ))}
