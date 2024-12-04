95eba0cce50051800b6a637f08b04d6945699597
Daniel <danielw@missionreadyhq.com>
2024-12-04 14:20:49 +1300

    added new Name input field with similar styling to jobTitle.

--- src/components/JobTitleInput.tsx
+++ src/components/JobTitleInput.tsx
@@ -8,18 +8,34 @@ export default function JobTitleInput({
   setJobTitle,
 }: JobTitleInputProps) {
   return (
-    <div className="mb-4">
-      <label htmlFor="job-title" className="text-xl text-[#0f9ed5] font-bold mr-2">
-        Job Title:
-      </label>
-      <input
-        type="text"
-        value={jobTitle}
-        onChange={(e) => setJobTitle(e.target.value)}
-        id="job-title"
-        name="job-title"
-        className="text-lg  font-medium border-[#0f9ed5] border-solid border-2 p-2 w-80 rounded-md focus:outline-none focus:ring-2 focus:ring-[#0f9ed5] focus:border-transparent transition duration-300"
-      />
+    <div className="mb-4 flex gap-8">
+      <div>
+        <label htmlFor="name" className="text-xl text-[#0f9ed5] font-bold mr-2">
+          Name:
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="text-lg font-medium border-[#0f9ed5] border-solid border-2 p-2 w-80 rounded-md focus:outline-none focus:ring-2 focus:ring-[#0f9ed5] focus:border-transparent transition duration-300"
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="job-title"
+          className="text-xl text-[#0f9ed5] font-bold mr-2"
+        >
+          Job Title:
+        </label>
+        <input
+          type="text"
+          value={jobTitle}
+          onChange={(e) => setJobTitle(e.target.value)}
+          id="job-title"
+          name="job-title"
+          className="text-lg  font-medium border-[#0f9ed5] border-solid border-2 p-2 w-80 rounded-md focus:outline-none focus:ring-2 focus:ring-[#0f9ed5] focus:border-transparent transition duration-300"
+        />
+      </div>
     </div>
   );
 }
